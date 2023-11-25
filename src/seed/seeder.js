90703122c97a8bb96a2123795d3be409e8949d74
Sebastian Llamuca <llamucasebas@gmail.com>
2023-11-25 16:24:45 -0500

    feat: add user seeder

--- src/seed/seeder.js
+++ src/seed/seeder.js
@@ -1,5 +1,7 @@
 import { exit } from 'node:process'
 import db from '../config/db.js'
+import User from '../model/User.js'
+import listUser from './user.seeder.js'
 import Customer from '../model/Customer.js'
 import listCustomer from './customer.seeder.js'
 
@@ -7,8 +9,15 @@ const importarDatos = async () => {
   try {
     await db.authenticate()
     await db.sync()
-    await Customer.destroy({ where: {} })
-    await Customer.bulkCreate(listCustomer)
+    await Promise.all([
+      Customer.destroy({ where: {} }),
+      User.destroy({ where: {} })
+    ])
+    console.log('Datos limpios')
+    await Promise.all([
+      User.bulkCreate(listUser),
+      Customer.bulkCreate(listCustomer)
+    ])
     console.log('Datos Importados correctamente')
     exit()
   } catch (error) {
---
+++ src/seed/user.seeder.js
@@ -0,0 +1,19 @@
+const listUser = [
+  {
+    name: 'Sebastian Llamuca',
+    email: 'llamucasebas@gmail.com',
+    password: '123456789'
+  },
+  {
+    name: 'Betsaly Solano',
+    email: 'betsalysolano@gmail.com',
+    password: '123456789'
+  },
+  {
+    name: 'Admin',
+    email: 'admin@admin.com',
+    password: '123456789'
+  }
+]
+
+export default listUser
