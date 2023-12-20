9ad2d8495ae995ce56eae81c9f9ba1612411ed64
Sebastian Llamuca <llamucasebas@gmail.com>
2023-12-20 00:57:15 -0500

    feat: add customer in seeders

--- src/seed/customer.seeder.js
+++ src/seed/customer.seeder.js
@@ -107,12 +107,204 @@ const listCustomer = [
     phone: '654321987',
     ruc: '45678901234'
   },
+  // asdasd
   {
-    name: 'Global Financial Services SA',
-    email: 'info@gfs.com',
-    address: 'Finance Avenue 101',
-    phone: '123789456',
+    name: 'Gherson Bruce',
+    lastName: 'Rivadeneira Aliaga',
+    email: 'bruce@example.com',
+    address: '456 Oak Avenue',
+    phone: '123456789',
+    identification: '789012345612'
+  },
+  {
+    name: 'Alice',
+    lastName: 'Johnson',
+    email: 'alice.johnson@example.com',
+    address: '123 Elm Street',
+    phone: '5551112233',
+    identification: '456789012345'
+  },
+  {
+    name: 'Bob',
+    lastName: 'Miller',
+    email: 'bob.miller@example.com',
+    address: '789 Cedar Avenue',
+    phone: '9876543210',
+    identification: '789012345678'
+  },
+  {
+    name: 'Eva',
+    lastName: 'Williams',
+    email: 'eva.williams@example.com',
+    address: '567 Birch Road',
+    phone: '1234567890',
+    identification: '234567890123'
+  },
+  {
+    name: 'David',
+    lastName: 'Brown',
+    email: 'david.brown@example.com',
+    address: '890 Pine Street',
+    phone: '5556667777',
+    identification: '678901234567'
+  },
+  {
+    name: 'Grace',
+    lastName: 'Taylor',
+    email: 'grace.taylor@example.com',
+    address: '234 Oak Lane',
+    phone: '1112223333',
+    identification: '345678901234'
+  },
+  {
+    name: 'Michael',
+    lastName: 'Clark',
+    email: 'michael.clark@example.com',
+    address: '567 Pine Avenue',
+    phone: '3334445555',
+    identification: '890123456789'
+  },
+  {
+    name: 'Samantha',
+    lastName: 'Walker',
+    email: 'samantha.walker@example.com',
+    address: '123 Birch Lane',
+    phone: '7778889999',
+    identification: '012345678901'
+  },
+  {
+    name: 'Daniel',
+    lastName: 'Wright',
+    email: 'daniel.wright@example.com',
+    address: '456 Cedar Road',
+    phone: '2223334444',
+    identification: '789012345678'
+  },
+  {
+    name: 'Olivia',
+    lastName: 'Roberts',
+    email: 'olivia.roberts@example.com',
+    address: '789 Oak Street',
+    phone: '4445556666',
+    identification: '901234567890'
+  },
+  {
+    name: 'Ryan',
+    lastName: 'Harrison',
+    email: 'ryan.harrison@example.com',
+    address: '890 Maple Lane',
+    phone: '6667778888',
+    identification: '123456789012'
+  },
+  {
+    name: 'Sophia',
+    lastName: 'Carter',
+    email: 'sophia.carter@example.com',
+    address: '234 Elm Avenue',
+    phone: '5556667777',
+    identification: '234567890123'
+  },
+  {
+    name: 'Ethan',
+    lastName: 'Baker',
+    email: 'ethan.baker@example.com',
+    address: '678 Cedar Street',
+    phone: '8889990000',
+    identification: '345678901234'
+  },
+  {
+    name: 'Ava',
+    lastName: 'Perez',
+    email: 'ava.perez@example.com',
+    address: '345 Pine Road',
+    phone: '1112223333',
+    identification: '456789012345'
+  },
+  {
+    name: 'Matthew',
+    lastName: 'Gonzalez',
+    email: 'matthew.gonzalez@example.com',
+    address: '456 Elm Lane',
+    phone: '9990001111',
+    identification: '567890123456'
+  },
+  {
+    name: 'Emma',
+    lastName: 'Fisher',
+    email: 'emma.fisher@example.com',
+    address: '789 Birch Street',
+    phone: '2223334444',
+    identification: '678901234567'
+  },
+  {
+    name: 'Data Solutions LLC',
+    email: 'info@datasolutions.com',
+    address: 'Data Street 456',
+    phone: '123456789',
+    ruc: '34567890123'
+  },
+  {
+    name: 'InnoTech Enterprises Inc.',
+    email: 'contact@innotech.com',
+    address: 'Innovation Avenue 789',
+    phone: '987654321',
+    ruc: '45678901234'
+  },
+  {
+    name: 'Global Dynamics Ltd.',
+    email: 'info@globaldynamics.com',
+    address: 'Global Square 567',
+    phone: '555666777',
     ruc: '56789012345'
+  },
+  {
+    name: 'Future Systems Corporation',
+    email: 'info@futuresystems.com',
+    address: 'Futuristic Lane 890',
+    phone: '111222333',
+    ruc: '67890123456'
+  },
+  {
+    name: 'TechHub Innovations',
+    email: 'contact@techhubinnovations.com',
+    address: 'Tech Street 123',
+    phone: '999000111',
+    ruc: '78901234567'
+  },
+  {
+    name: 'EcoSolutions Group',
+    email: 'info@ecosolutionsgroup.com',
+    address: 'Eco Avenue 234',
+    phone: '333444555',
+    ruc: '89012345678'
+  },
+  {
+    name: 'Strategic Technologies Ltd.',
+    email: 'contact@strategictech.com',
+    address: 'Strategy Lane 345',
+    phone: '666777888',
+    ruc: '90123456789'
+  },
+  {
+    name: 'InnoData Systems Inc.',
+    email: 'info@innodata.com',
+    address: 'Inno Square 678',
+    phone: '777888999',
+    ruc: '01234567890'
+  },
+  {
+    name: 'Synthetics Innovate LLC',
+    email: 'info@syntheticsinnovate.com',
+    address: 'Synthetics Street 901',
+    phone: '444555666',
+    ruc: '12345678901'
+  },
+  {
+    name: 'NexGen Solutions Ltd.',
+    email: 'contact@nexgensolutions.com',
+    address: 'NexGen Avenue 234',
+    phone: '222333444',
+    ruc: '23456789012'
   }
 ]
 
