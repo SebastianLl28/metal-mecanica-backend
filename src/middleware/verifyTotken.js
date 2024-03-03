a79a8511be4a031eb18c34bb475da964a0f212e6
Sebastian Llamuca <llamucasebas@gmail.com>
2024-03-02 20:18:12 -0500

    refactor: delete console log

--- src/middleware/verifyTotken.js
+++ src/middleware/verifyTotken.js
@@ -11,13 +11,10 @@ export const verifyTotken = async (req, res, next) => {
     const authHeader = req.headers.authorization
     if (!authHeader) return res.status(401).json({ message: 'Unauthorized' })
     const token = authHeader.split(' ')[1]
-    console.log(token)
     const payload = verifyTokenSecretKey(token)
     if (!payload) return res.status(401).json({ message: 'Unauthorized' })
     const findUser = await findUserById(payload.id)
-    console.log('llego 2')
     if (!findUser) return res.status(401).json({ message: 'Unauthorized' })
-    console.log('llego 1')
     req.user = findUser
     next()
   } catch (error) {
