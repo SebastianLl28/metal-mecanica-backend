e1dac22474e98fc7c62fc6c9b93803d777496a9a
Sebastian Llamuca <llamucasebas@gmail.com>
2024-03-14 15:49:13 -0500

    feat(customer router): :sparkles: add verify token in the routers

--- src/router/customer.router.js
+++ src/router/customer.router.js
@@ -8,13 +8,19 @@ import {
 } from '../controller/customer.controller.js'
 import { schemeValidation } from '../middleware/schemeValidator.js'
 import { registerCustomerSchema } from '../schema/customer.schema.js'
+import { verifyTotken } from '../middleware/verifyTotken.js'
 
 const router = Router()
 
-router.get('/', getfindAll)
-router.get('/:id', getfindById)
-router.post('/', schemeValidation(registerCustomerSchema), postCreate)
-router.put('/:id', putUpdate)
-router.delete('/:id', deleteDeleteById)
+router.get('/', verifyTotken, getfindAll)
+router.get('/:id', verifyTotken, getfindById)
+router.post(
+  '/',
+  verifyTotken,
+  schemeValidation(registerCustomerSchema),
+  postCreate
+)
+router.put('/:id', verifyTotken, putUpdate)
+router.delete('/:id', verifyTotken, deleteDeleteById)
 
 export default router
